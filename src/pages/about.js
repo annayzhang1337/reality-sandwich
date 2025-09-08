6e264c1ffbe4795f8ad7981ba80a681b1695a815
Anna Zhang <yutongzhang2001@gmail.com>
2025-09-08 15:46:47 -0700

    live laugh love

--- src/pages/about.js
+++ src/pages/about.js
@@ -18,6 +18,7 @@ export function renderAbout() {
             <li><strong>The Best Moat is a Point of View.</strong> In a world of commoditized technology, the most defensible advantage is a unique, hard-won, and deeply-held worldview. Great products are not just code; they are philosophy embedded in software.</li>
             <li><strong>Human Stories are the Only Ones That Matter.</strong> Technology is not the end. It is a tool. The goal is not to optimize humanity, but to create more space for its messy, beautiful, and inefficient truths.</li>
             <li><strong>The Work is to See the World as It Is.</strong> ...and then have the courage to build the world as it should be.</li>
+            <li><strong>Don't Take Yourself Too Seriously.</strong> At the end of the day, it's all a beautiful, absurd, and <i>infinite</i> game. The ability to laugh at your own ambition is the final, and most important, form of freedom.</li>
           </ul>
         </div>
         
